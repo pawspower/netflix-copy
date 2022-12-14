a58e0a0e7af65d6aa8a55d60df90759add22bbbc
Keshia Wijaya <kesh.learning@gmail.com>
2022-12-14 13:01:20 +1100

    Wednesday14Dec, second commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,9 +23,9 @@ const Main = () => {
 
 
   return (
-    <div className='w-full h-[700px] text-white'>
+    <div className='w-full h-[600px] text-white'>
       <div className='w-full h-full'>
-        <div className='absolute w-full h-[700px] bg-gradient-to-r from-black'></div>
+        <div className='absolute w-full h-[600px] bg-gradient-to-r from-black'></div>
         <img
           className='w-full h-full object-cover'
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
